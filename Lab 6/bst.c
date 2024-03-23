7dbbd34c54392c4a4f16c294b96e5157334727f6
Ram <panpaliaram@gmail.com>
2024-03-23 19:54:27 +0530

    gitignore added

--- Lab 6/bst.c
+++ Lab 6/bst.c
@@ -1,17 +1,17 @@
+#include <limits.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <limits.h>
 
-int max(int a, int b){
-    if(a>b) return a;
-    return b;
-}
+// int max(int a, int b){
+//     if(a>b) return a;
+//     return b;
+// }
 
-int min(int a, int b){
-    if(a<b) return a;
-    return b;
-}
+// int min(int a, int b){
+//     if(a<b) return a;
+//     return b;
+// }
 
 typedef struct node {
     int value;
@@ -19,30 +19,28 @@ typedef struct node {
     struct node *right;
 } Node;
 
+// typedef struct node Node;
+
 typedef struct bst {
     Node *root;
 } BST;
 
-BST *new_bst() 
-{
+BST *new_bst() {
     BST *bst = malloc(sizeof(BST));
     bst->root = NULL;
     return bst;
 }
 
-Node *new_node(int value) 
-{
-    Node *node = malloc(sizeof(Node));
-    node->value = value;
-    node->left = NULL;
-    node->right = NULL;
-    return node;
+Node *new_node(int value) {
+    Node *nd = malloc(sizeof(Node));
+    nd->value = value;
+    nd->left = NULL;
+    nd->right = NULL;
+    return nd;
 }
 
-void traverse_in_order(Node *node)
-{
-    if (node == NULL)
-    {
+void traverse_in_order(Node *node) {
+    if (node == NULL) {
         printf("null ");
         return;
     }
@@ -51,10 +49,8 @@ void traverse_in_order(Node *node)
     traverse_in_order(node->right);
 }
 
-void traverse_pre_order(Node *node)
-{
-    if (node == NULL)
-    {
+void traverse_pre_order(Node *node) {
+    if (node == NULL) {
         printf("null ");
         return;
     }
@@ -63,10 +59,8 @@ void traverse_pre_order(Node *node)
     traverse_pre_order(node->right);
 }
 
-void traverse_post_order(Node *node)
-{
-    if (node == NULL)
-    {
+void traverse_post_order(Node *node) {
+    if (node == NULL) {
         printf("null ");
         return;
     }
@@ -75,30 +69,22 @@ void traverse_post_order(Node *node)
     printf("%d ", node->value);
 }
 
-void insert(BST *bst, int value)
-{
+void insert(BST *bst, int value) {
     Node *node = new_node(value);
-    if (bst->root == NULL) 
-    {
+    if (bst->root == NULL) {
         bst->root = node;
         return;
     }
     Node *current = bst->root;
-    while (current != NULL) 
-    {
-        if (value < current->value) 
-        {
-            if (current->left == NULL) 
-            {
+    while (current != NULL) {
+        if (value < current->value) {
+            if (current->left == NULL) {
                 current->left = node;
                 return;
             }
             current = current->left;
-        } 
-        else 
-        {
-            if (current->right == NULL) 
-            {
+        } else {
+            if (current->right == NULL) {
                 current->right = node;
                 return;
             }
@@ -107,156 +93,123 @@ void insert(BST *bst, int value)
     }
 }
 
-int mxValBST(Node* root){
-    if(root==NULL) return INT_MIN;
-    int maxi=max(mxValBST(root->left),mxValBST(root->right));
-    return max(root->value,maxi);
+int mxValBST(Node *root) {
+    if (root == NULL) return 0;
+    if(root->right==NULL) return root->value;
+    return mxValBST(root->right);
 }
 
-int mnValBST(Node* root){
-    if(root==NULL) return INT_MAX;
-    int mini=min(mnValBST(root->left),mnValBST(root->right));
-    return min(root->value,mini);
+int mnValBST(Node *root) {
+    if (root == NULL) return 0;
+    if(root->left==NULL) return root->value;
+    return mnValBST(root->left);
 }
 
-bool isValidBST(Node* root){
-    if(root==NULL) return true;
-    if(root->value>=mxValBST(root->left) && root->value<=mnValBST(root->right)){
+bool isValidBST(Node *root) {
+    if (root == NULL) return true;
+    if (root->value >= mxValBST(root->left) &&
+        root->value <= mnValBST(root->right)) {
         return isValidBST(root->left) && isValidBST(root->right);
     }
     return false;
 }
 
-int search(BST *bst, int key)
-{
+int search(BST *bst, int key) {
     Node *current = bst->root;
-    while (current != NULL) 
-    {
-        if (key == current->value) 
-        {
+    while (current != NULL) {
+        if (key == current->value) {
             return 1;
-        } 
-        else if (key < current->value) 
-        {
+        } else if (key < current->value) {
             current = current->left;
-        } 
-        else 
-        {
+        } else {
             current = current->right;
         }
     }
     return 0;
 }
 
-int find_min(BST *bst)
-{
+int find_min(BST *bst) {
     Node *current = bst->root;
-    while (current->left != NULL) 
-    {
+    while (current->left != NULL) {
         current = current->left;
     }
     return current->value;
 }
 
-int find_max(BST *bst)
-{
+int find_max(BST *bst) {
     Node *current = bst->root;
-    while (current->right != NULL) 
-    {
+    while (current->right != NULL) {
         current = current->right;
     }
     return current->value;
 }
 
-Node *predecessor(Node *node)
-{
-    if (node->left == NULL)
-    {
+Node *predecessor(Node *node) {
+    if (node->left == NULL) {
         return NULL;
     }
     Node *current = node->left;
-    while (current->right != NULL)
-    {
+    while (current->right != NULL) {
         current = current->right;
     }
     return current;
 }
 
-Node *successor(Node *node)
-{
-    if (node->right == NULL)
-    {
+Node *successor(Node *node) {
+    if (node->right == NULL) {
         return NULL;
     }
     Node *current = node->right;
-    while (current->left != NULL)
-    {
+    while (current->left != NULL) {
         current = current->left;
     }
     return current;
 }
 
-void delete(BST *bst, Node *node)
-{
-    if (node == NULL)
-        return;
-    if (node->left == NULL && node->right == NULL)
-    {
+void delete(BST *bst, Node *node) {
+    if (node == NULL) return;
+    if (node->left == NULL && node->right == NULL) {
         // Node is a leaf
-        Node* current = bst->root;
-        while (current != NULL)
-        {
-            if (current->left == node)
-            {
+        Node *current = bst->root;
+        while (current != NULL) {
+            if (current->left == node) {
                 current->left = NULL;
                 break;
             }
-            if (current->right == node)
-            {
+            if (current->right == node) {
                 current->right = NULL;
                 break;
             }
-            if (node->value < current->value)
-            {
+            if (node->value < current->value) {
                 current = current->left;
-            }
-            else
-            {
+            } else {
                 current = current->right;
             }
         }
         free(node);
         return;
     }
-    
-    if (node->left == NULL)
-    {
+
+    if (node->left == NULL) {
         // Node only has right child
-        Node* current = bst->root;
-        if (current == node)
-        {
+        Node *current = bst->root;
+        if (current == node) {
             bst->root = node->right;
             free(node);
             return;
         }
-        while (current != NULL)
-        {
-            if (current->left == node)
-            {
+        while (current != NULL) {
+            if (current->left == node) {
                 current->left = node->right;
                 break;
             }
-            if (current->right == node)
-            {
+            if (current->right == node) {
                 current->right = node->right;
                 break;
             }
-            if (node->value < current->value)
-            {
+            if (node->value < current->value) {
                 current = current->left;
-            }
-            else
-            {
+            } else {
                 current = current->right;
             }
         }
@@ -264,34 +217,26 @@ void delete(BST *bst, Node *node)
         return;
     }
 
-    if (node->right == NULL)
-    {
+    if (node->right == NULL) {
         // Node only has left child
-        Node* current = bst->root;
-        if (current == node)
-        {
+        Node *current = bst->root;
+        if (current == node) {
             bst->root = node->left;
             free(node);
             return;
         }
-        while (current != NULL)
-        {
-            if (current->left == node)
-            {
+        while (current != NULL) {
+            if (current->left == node) {
                 current->left = node->left;
                 break;
             }
-            if (current->right == node)
-            {
+            if (current->right == node) {
                 current->right = node->left;
                 break;
             }
-            if (node->value < current->value)
-            {
+            if (node->value < current->value) {
                 current = current->left;
-            }
-            else
-            {
+            } else {
                 current = current->right;
             }
         }
@@ -302,13 +247,13 @@ void delete(BST *bst, Node *node)
     // Node has both children
     Node *temp = successor(node);
     node->value = temp->value;
-    delete(bst, temp);
+    delete (bst, temp);
     return;
 }
 
-int height(Node* root){
-    if(root==NULL) return 0;
-    return 1+max(height(root->left),height(root->right));
+int height(Node *root) {
+    if (root == NULL) return 0;
+    return 1 + max(height(root->left), height(root->right));
 }
 
 // void giveMuktiToAllChildNodes(Node *atma){
@@ -333,39 +278,37 @@ int height(Node* root){
 //     if(root->left!=NULL) giveMuktiToAllChildNodes(root->left);
 // }
 
-
-
-void freeAll(Node *node){
-    if(node==NULL) return;
+void freeAll(Node *node) {
+    if (node == NULL) return;
     freeAll(node->left);
     freeAll(node->right);
-    node=NULL;
+    node = NULL;
     free(node);
     return;
 }
 
-void removeHalfNodes(Node* root){
-    if(root==NULL) return;
-    if(root->left!=NULL && root->right!=NULL){
+void removeHalfNodes(Node *root) {
+    if (root == NULL) return;
+    if (root->left != NULL && root->right != NULL) {
         removeHalfNodes(root->left);
         removeHalfNodes(root->right);
         return;
     }
-    if(root->right!=NULL) freeAll(root->right);
-    if(root->left!=NULL) freeAll(root->left);
+    if (root->right != NULL) freeAll(root->right);
+    if (root->left != NULL) freeAll(root->left);
 }
 
-BST* constructBST(int *arrey, int n){
+BST *constructBST(int *arrey, int n) {
     BST *bst = new_bst();
-    for(int i=0;i<n;i++) insert(bst,arrey[i]);
+    for (int i = 0; i < n; i++) insert(bst, arrey[i]);
     return bst;
 }
 
-// Driver program to test the above functions (feel free to play around with this)
-int main()
-{
-    int array[] = {20,21,1,2,3,4,5,7};
-    BST *bst = constructBST(array,8);
+// Driver program to test the above functions (feel free to play around with
+// this)
+int main() {
+    int array[] = {20, 21, 1, 2, 3, 4, 5, 7};
+    BST *bst = constructBST(array, 8);
     // BST *bst = new_bst();
     // insert(bst, 2);
     // insert(bst, 1);
@@ -401,7 +344,7 @@ int main()
     traverse_in_order(bst->root);
     printf(" <= InOrderTraversal after removing halfNodes\n");
 
-    printf("%s",isValidBST(bst->root)?"Valid":"InValid");
+    printf("%s", isValidBST(bst->root) ? "Valid" : "InValid");
     // delete(bst, bst->root->left);
     // traverse_in_order(bst->root);
     // printf("\n");
